0c8a4c20d5c7b1ce7fa9d2da72aa9d7fcdaeaca1
Anthony Okpako <melowsworld@yahoo.co.uk>
2018-04-11 17:37:11 +0100

    this is my first commit

---
+++ myhome.php
@@ -0,0 +1,16 @@
+<DOCTYPE! html>
+<html>
+    <head>
+        
+        <title>My Home page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+       
+    </head>
+    <body>
+        <div>
+            <h1>Welcome to <br>My <br>Home Page </h1>
+        <h1> <?php echo date("h:ia") ?> </h1>    
+        </div>
+    </body>
+</html>
